e55687c2293e02bef6334743bbf691dd1645b189
M Starch <lestarch@googlemail.com>
2017-01-04 19:56:01 -0800

    lestarch: initial work on lesson 1

---
+++ reference/base-files/www/js/gwc-main.js
@@ -0,0 +1,18 @@
+/**
+ * This file contains the entry point for the gwc app project code. The initialization of
+ * the Cordova App is a bit complex, and has been configured to point to the main function
+ * in this file.  This lowers the leraning curev for our students.
+ *
+ * @author lestarch (Michael Starch)
+ */
+/**
+ * Main function entry point for GWC project code. This function runs after the app has 
+ * been initialized and all resources are prepared for execution.  It is passed the 
+ * "app" object from cordova in case that is needed.
+ * @param app - Cordova app object
+ */
+function main(app)
+{
+    //Start your GWC code here
+
+}
